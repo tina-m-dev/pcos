b3f4b0b4e0bc0e7bfe70be614a946d9cb9d18721
Tina Martinić <124673402+tina-m-dev@users.noreply.github.com>
2024-06-16 21:26:26 +0200

    Update your_script.sql

--- your_script.sql
+++ your_script.sql
@@ -1,7 +1,8 @@
 CREATE TABLE pcos_infertility (
     "Sl. No" INTEGER PRIMARY KEY,
-    Patient File No.,
-    PCOS (Y/N),
-    I   beta-HCG(mIU/mL),
-    II    beta-HCG(mIU/mL),AMH(ng/mL)
+    "Patient File No.",
+    "PCOS (Y/N)",
+    "I   beta-HCG(mIU/mL)",
+    "II    beta-HCG(mIU/mL)",
+    "AMH(ng/mL)"
 );
