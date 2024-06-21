937924d32d18918d601e8687c68c5fadc83b86a5
Tina Martinić <124673402+tina-m-dev@users.noreply.github.com>
2024-06-21 14:49:35 +0200

    Update query_postgres.py

--- query_postgres.py
+++ query_postgres.py
@@ -7,9 +7,7 @@ db_url = 'postgresql://user:password@localhost:5432/mydatabase'
 # Function to list all tables in the public schema
 def list_tables(engine):
     query = '''
-        SELECT Sl.No
-        ,BMI
-        ,FSH/LH
+        SELECT "Sl.No"
         FROM pcos
         
     '''
