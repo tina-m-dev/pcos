a6f9f24d9853d80747312006082e8ccde147ec24
Tina Martinić <124673402+tina-m-dev@users.noreply.github.com>
2024-06-22 11:13:00 +0200

    Update query_postgres.py

--- query_postgres.py
+++ query_postgres.py
@@ -7,22 +7,17 @@ db_url = 'postgresql://user:password@localhost:5432/mydatabase'
 # Function to list all tables in the public schema
 def list_tables(engine):
     query = '''
-    SELECT pcos."Sl. No"
-        ,pcos."BMI"
-        ,"FSH/LH"
-    FROM pcos
-        
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = 'public'
+    ORDER BY table_name
     '''
     
     with engine.connect() as conn:
         result = conn.execute(text(query))
-        tables_df = pd.DataFrame(result.fetchall(), columns=result.keys())
+        tables = [row[0] for row in result.fetchall()]
     
-    # Write to output file
-    with open('pcos_data/output.txt', 'w') as f:
-        f.write("Tables in the database:\n")
-        f.write(tables_df.to_string(index=False))
-        f.write("\n\n")
+    return tables
 
 # Function to perform a SELECT query on a specific table
 def select_from_table(engine, table_name):
@@ -32,20 +27,27 @@ def select_from_table(engine, table_name):
         result = conn.execute(query)
         df = pd.DataFrame(result.fetchall(), columns=result.keys())
     
-    # Append to output file
-    with open('pcos_data/output.txt', 'a') as f:
-        f.write(f"Data from table {table_name}:\n")
-        f.write(df.to_string(index=False))
-        f.write("\n\n")
+    return df
 
 # Main function to run the script
 def main():
     engine = create_engine(db_url)
     
     # List all tables
-    list_tables(engine)
+    tables = list_tables(engine)
     
-   
+    # Write to output file
+    with open('pcos_data/output.txt', 'w') as f:
+        f.write("Tables in the database:\n")
+        f.write(", ".join(tables))
+        f.write("\n\n")
+        
+        # Perform SELECT queries on each table
+        for table in tables:
+            df = select_from_table(engine, table)
+            f.write(f"Data from table {table}:\n")
+            f.write(df.to_string(index=False))
+            f.write("\n\n")
 
 if __name__ == "__main__":
     main()
