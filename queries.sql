e92ba908bda9968659ae257dc8377cf4bf63d822
Tina Martinić <124673402+tina-m-dev@users.noreply.github.com>
2024-06-26 23:32:35 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,5 +1,11 @@
-SELECT *
-FROM pcos;
-
-SELECT *
-FROM pcos
+DO $$
+DECLARE
+    query_text TEXT;
+BEGIN
+    SELECT 'SELECT * FROM pcos WHERE ' || string_agg(column_name || ' IS NULL', ' OR ')
+    INTO query_text
+    FROM information_schema.columns
+    WHERE table_name = 'pcos';
+    
+    EXECUTE query_text;
+END $$;
