c55891086aaaf27a70d3cdc4a14a8d7d6487ce15
Tina Martinić <124673402+tina-m-dev@users.noreply.github.com>
2024-06-27 11:00:30 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,7 +6,8 @@ BEGIN
            string_agg('"' || column_name || '" IS NULL', ' OR ')
     INTO query_text
     FROM information_schema.columns
-    WHERE table_name = 'pcos' ; -- Adjust schema if necessary
+    WHERE table_name = 'pcos' AND table_schema = 'public'; -- Adjust schema if necessary
     
+    RAISE NOTICE 'Executing query: %', query_text; -- Log the query being executed
     EXECUTE query_text;
 END $$;
