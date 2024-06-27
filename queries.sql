0e6273fbacedca2b53d40c2cf1ae3aef39019ecd
Tina Martinić <124673402+tina-m-dev@users.noreply.github.com>
2024-06-27 11:40:16 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,30 +2,6 @@ DO $$
 DECLARE
     query_text TEXT;
     result RECORD;
-BEGIN
-    -- Generate the dynamic query
-    SELECT 'SELECT * FROM pcos WHERE ' || 
-           string_agg('"' || column_name || '" IS NULL', ' OR ')
-    INTO query_text
-    FROM information_schema.columns
-    WHERE table_name = 'pcos' AND table_schema = 'public'; -- Adjust schema if necessary
-    
-    RAISE NOTICE 'Executing query: %', query_text; -- Log the query being executed
-
-    -- Open a cursor to execute the dynamic query
-    FOR result IN EXECUTE query_text
-    LOOP
-        -- Display each row
-        RAISE NOTICE '%', row_to_json(result);
-    END LOOP;
-END $$;
-
-
-
-DO $$
-DECLARE
-    query_text TEXT;
-    result RECORD;
     col_name TEXT;
     tbl_name TEXT;
 BEGIN
@@ -52,4 +28,31 @@ BEGIN
     END LOOP;
 END $$;
 
+
+
+/*
+
+DO $$
+DECLARE
+    query_text TEXT;
+    result RECORD;
+BEGIN
+    -- Generate the dynamic query
+    SELECT 'SELECT * FROM pcos WHERE ' || 
+           string_agg('"' || column_name || '" IS NULL', ' OR ')
+    INTO query_text
+    FROM information_schema.columns
+    WHERE table_name = 'pcos' AND table_schema = 'public'; -- Adjust schema if necessary
+    
+    RAISE NOTICE 'Executing query: %', query_text; -- Log the query being executed
+
+    -- Open a cursor to execute the dynamic query
+    FOR result IN EXECUTE query_text
+    LOOP
+        -- Display each row
+        RAISE NOTICE '%', row_to_json(result);
+    END LOOP;
+END $$;
+
+*/
 
