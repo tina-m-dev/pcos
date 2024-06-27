7f578fc3247f065254d24b822ff4eefaf9dcc591
Tina Martinić <124673402+tina-m-dev@users.noreply.github.com>
2024-06-27 10:50:47 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,11 +2,11 @@ DO $$
 DECLARE
     query_text TEXT;
 BEGIN
-    SELECT 'SELECT * FROM pcos_db WHERE ' || 
+    SELECT 'SELECT * FROM pcos WHERE ' || 
            string_agg('"' || column_name || '" IS NULL', ' OR ')
     INTO query_text
     FROM information_schema.columns
-    WHERE table_name = 'pcos_db' AND table_schema = 'public'; -- Adjust schema if necessary
+    WHERE table_name = 'pcos' AND table_schema = 'public'; -- Adjust schema if necessary
     
     EXECUTE query_text;
 END $$;
